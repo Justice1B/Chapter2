56afecf651344fe9b3423f1efff3111953a84623
lt9e <lt9e@fresnou.org>
2023-02-17 14:17:32 -0800

    Added whileLoops1.js

---
+++ whileLoops1.js
@@ -0,0 +1,25 @@
+const readline = require('readline-sync');
+
+// let y = -1;
+// while (y < 25){
+//    y++;
+//    console.log(y);
+// }
+
+// let w = -1;
+// while (w < 50){
+//    w++;
+//    console.log(w);
+// }
+
+// let x = -11;
+// while (x < 10){
+//    x++;
+//    console.log(x);
+// }
+
+let y = 1;
+while (y > -50){
+    y--;
+    console.log(y);
+}
