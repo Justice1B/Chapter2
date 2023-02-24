1456ca339aa5298ec511ffa6e408d2d2b0bb165d
lt9e <lt9e@fresnou.org>
2023-02-24 13:39:12 -0800

    Added forLoops1.js

--- forLoops1.js
+++ forLoops1.js
@@ -1,98 +1,98 @@
-// For Loops
-for (let i=0; i <= 10; i++ ){
-   console.log("hi");
-}
+//For Loops
+// for (let i=0; i <= 10; i++ ){
+//    console.log("hi");
+// }
 
-for (let i=0; i <= 10; i++ ){
-   console.log(i);
-}
+// for (let i=0; i <= 10; i++ ){
+//    console.log(i);
+// }
 
-for (let i = 0; i <= 1001; i++ ){
-   console.log(i);
-}
+// for (let i = 0; i <= 1001; i++ ){
+//    console.log(i);
+// }
 
-for (let i = 100; i >=12; i-- ){
-    console.log(i);
-}
+// for (let i = 100; i >=12; i-- ){
+//     console.log(i);
+// }
 
-for (let i = 100; i <=300; i++ ){
-    if (i % 2 == 0) {
-        console.log(i);
-    }
-}
+// for (let i = 100; i <=300; i++ ){
+//     if (i % 2 == 0) {
+//         console.log(i);
+//     }
+// }
 
-let sum = 0;
-for (let i = 0; i <= 100; i++ ){
-    sum += i;
-}
-console.log(sum);
+// let sum = 0;
+// for (let i = 0; i <= 100; i++ ){
+//     sum += i;
+// }
+// console.log(sum);
 
-let sum = 0;
-for (let i = 0; i <= 100; i++ ){
-    if (i % 2 == 1) {
-        sum += i;
-    }
-}
-console.log(sum);
+// let sum = 0;
+// for (let i = 0; i <= 100; i++ ){
+//     if (i % 2 == 1) {
+//         sum += i;
+//     }
+// }
+// console.log(sum);
 
 //1
-for (let i=0; i <= 150; i++ ){
-   console.log(i);
-}
+// for (let i=0; i <= 150; i++ ){
+//    console.log(i);
+// }
 
-// 2
-for (let i=1; i <= 40; i++ ){
-   console.log(i * i);
-}
+//2
+// for (let i=1; i <= 40; i++ ){
+//    console.log(i * i);
+// }
 
-// 3
-for (let i=1; i <= 40; i++ ){
-    console.log(i * i * i);
-}
+//3
+// for (let i=1; i <= 40; i++ ){
+//     console.log(i * i * i);
+// }
 
-// 4
-for (let i = 0; i <=700; i++ ){
-    if (i % 5 == 0) {
-        console.log(i);
-    }
-}
+//4
+// for (let i = 0; i <=700; i++ ){
+//     if (i % 5 == 0) {
+//         console.log(i);
+//     }
+// }
 
-// 5
-for (let i = 350; i <=1000; i++ ){
-    if (i % 10 == 0) {
-        console.log(i);
-    }
-}
+//5
+// for (let i = 350; i <=1000; i++ ){
+//     if (i % 10 == 0) {
+//         console.log(i);
+//     }
+// }
 
-// 6
-let sum = 0;
-for (let i = 1; i <= 50; i++ ){
-    sum += i;
-}
-console.log(sum);
+//6
+// let sum = 0;
+// for (let i = 1; i <= 50; i++ ){
+//     sum += i;
+// }
+// console.log(sum);
 
-// 7
-let sum = 0;
-for (let i = 35; i <= 70; i++ ){
-    sum += i;
-}
-console.log(sum);
+//7
+// let sum = 0;
+// for (let i = 35; i <= 70; i++ ){
+//     sum += i;
+// }
+// console.log(sum);
 
-// 8
-let sum = 1;
-for (let i = 1; i <= 15; i++ ){
-    sum *= i;
-}
-console.log(sum);
+//8
+// let sum = 1;
+// for (let i = 1; i <= 15; i++ ){
+//     sum *= i;
+// }
+// console.log(sum);
 
-// 9
-let sum = 1;
-for (let i = 10; i <= 35; i++ ){
-    sum *= i;
-}
-console.log(sum);
+//9
+// let sum = 1;
+// for (let i = 10; i <= 35; i++ ){
+//     sum *= i;
+// }
+// console.log(sum);
 
-// 10
-for (let i=0; i <= 12; i++ ){
-   console.log("Happy Holidays!");
+//10
+ for (let i=0; i <= 12; i++ ){
+    console.log("Happy Holidays!");
 }
